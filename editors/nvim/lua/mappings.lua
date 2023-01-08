6fac0eb1c3ef634acd221bcf26a11569f4ae7516
Janrey Licas <janjan.licas@gmail.com>
2023-01-08 18:27:38 +0800

    feat(nvim-config): follow-up commit on adding `mappings.lua`

---
+++ editors/nvim/lua/mappings.lua
@@ -0,0 +1,9 @@
+-- mappings.lua  | Literally anything mapping, specially keybinds and other suchs.
+-- Version 0.1.1 | Since 01/04/2023
+-- @CodexLink    | https://github.com/CodexLink
+
+local vg = vim.g
+local vkm = vim.keymap
+
+vg.mapleader = "\\"
+
