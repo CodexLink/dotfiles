53922b53b4aa03de45c12ed4cf3c5979a874efef
Janrey Licas <janjan.licas@gmail.com>
2023-01-17 16:42:31 +0800

    refactor(mappings): respect `key-notation` from combinatory binds

--- editors/nvim/lua/mappings.lua
+++ editors/nvim/lua/mappings.lua
@@ -8,14 +8,15 @@
 local wk = require("which-key")
 
 wk.register({
-		["<F1>"] = { [[ <CMD>Neotree action=show position=left reveal=true toggle=true<CR> ]], "neotree.nvim: Toggle" },
+		["<F1>"] = { [[ <CMD>Neotree action=focus position=left reveal=true toggle=true<CR> ]], "neotree.nvim: Toggle" },
 		["<F2>"] = { [[ <CMD>TroubleToggle<CR> ]], "trouble.nvim (Diagnostics): Toggle" },
 		["<F3>"] = "TODO: DAP",
 		["<F4>"] = { [[ <CMD>WhichKey<CR> ]], "which-key.nvim: Opens UI window for hinting keybinds" },
 		["<F5>"] = { [[ <CMD>Lazy<CR> ]], "lazy.nvim: Opens UI window" },
 		["<F6>"] = { [[ <CMD>Mason<CR> ]], "mason.nvim: Opens UI window" },
 		["<Leader>"] = {
-			ac = { [[ <CMD>Neogen<CR> ]], "neogen: Annotate code context" },
+			a = { [[ <CMD>Neogen<CR> ]], "neogen: Annotate code context" },
+			c = { [[ <CMD>TSContextToggle<CR> ]], "treesitter-context: Toggle" },
 			G = {
 				name = "gitsigns.nvim",
 				b = { [[ <CMD>Gitsigns toggle_current_line_blame<CR> ]], "Toggle line blame" },
@@ -23,12 +24,11 @@ wk.register({
 				D = { function() require("gitsigns").diffthis("~") end, "`diffthis` on whole file" },
 				h = { [[ <CMD>Gitsigns preview_hunk<CR> ]], "Preview hunk (changes on certain code line)" },
 			},
-			sr = { function () require("ssr").open() end, mode={ "n", "x" }, "ssr.nvim: Do 'Structural Search and Replace'" },
-			tc = { [[ <CMD>TSContextToggle<CR> ]], "treesitter-context: Toggle" },
-			th = { function() require("tsht").nodes() end, "nvim-treehopper: Toggle context to hoppable" },
-			i = { [[ <CMD>IncRename<CR> ]], "inc-rename.nvim: Rename on cursor" },
+			i = { ":IncRename ", "inc-rename.nvim: Rename on cursor" },
 			I = { function() return ":IncRename " .. vim.fn.expand("<CWORD>") end, "inc-rename.nvim: Rename by type", expr = true },
 			m = { [[ <CMD>MarkdownPreviewToggle<CR> ]], "markdown-preview.nvim: Toogle" },
+			s = { function () require("ssr").open() end, mode={ "n", "x" }, "ssr.nvim: Do 'Structural Search and Replace'" },
+			t = { function() require("tsht").nodes() end, "nvim-treehopper: Toggle context to hoppable" },
 			w = {
 				name = "windows.nvim",
 				["="] = { [[ <CMD>WindowsEqualize<CR> ]], "Equalize" },
@@ -37,15 +37,11 @@ wk.register({
 				["_"] = { [[ <CMD>WindowsMaximizeHorizontally<CR> ]], "Maximize horizontally" },
 			}
 		},
-		["<M>"] = {
-			[">"] = { [[ <CMD>BufferLineCyclePrev<CR> ]], "bufferline: Move to Previous Buffer." },
-			["<"] = { [[ <CMD>BufferLineCycleNext<CR> ]], "bufferline: Move to Next Buffer." },
-			W = { "<CMD>BufferLinePickClose<CR>", "bufferline: Pick a buffer to close." }, 	-- [1]
-			Q = { "<CMD>BufferLinePick<CR>", "bufferline: Pick a buffer to display." },			-- [1]
-			z = { [[ <CMD>TSJToggle<CR> ]], "treesj: Toggle 'One-Liner/Splitted' Style." }
-		},
-		["<S>"] = {
-			["<Left>"] = { [[ <CMD>BufferLineMovePrev<CR> ]], "bufferline: Move Buffer to the Left." },
-			["<Right>"] = { [[ <CMD>BufferLineMoveNext<CR> ]], "bufferline: Move Buffer to the Right." }
-		}
+		["M->"] = { [[ <CMD>BufferLineCyclePrev<CR> ]], "bufferline: Move to Previous Buffer." },
+		["M-<"] = { [[ <CMD>BufferLineCycleNext<CR> ]], "bufferline: Move to Next Buffer." },
+		["M-W"] = { "<CMD>BufferLinePickClose<CR>", "bufferline: Pick a buffer to close." }, 	-- [1]
+		["M-Q"] = { "<CMD>BufferLinePick<CR>", "bufferline: Pick a buffer to display." },			-- [1]
+		["M-z"] = { [[ <CMD>TSJToggle<CR> ]], "treesj: Toggle 'One-Liner/Splitted' Style." },
+		["<S-Left>"] = { [[ <CMD>BufferLineMovePrev<CR> ]], "bufferline: Move Buffer to the Left." },
+		["<S-Right>"] = { [[ <CMD>BufferLineMoveNext<CR> ]], "bufferline: Move Buffer to the Right." }
 	})
