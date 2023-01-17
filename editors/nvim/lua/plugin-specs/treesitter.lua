11d3b955fb80ec160c649adfabdaa9fcbf6b27a0
Janrey Licas <janjan.licas@gmail.com>
2023-01-18 02:46:35 +0800

    fix(treesitter): use version=`false` for all package with very stale release

--- editors/nvim/lua/plugin-specs/treesitter.lua
+++ editors/nvim/lua/plugin-specs/treesitter.lua
@@ -2,8 +2,9 @@
 -- @CodexLink    		| https://github.com/CodexLink
 
 -- Info
--- [1] According to the issue (https://github.com/LazyVim/LazyVim/issues/2) from LazyVim, there were build failures and repeating compilation done by `nvim-treesitter`.
+-- [1] According to the issue (https://github.com/LazyVim/LazyVim/issues/2) from LazyVim, there were build failures and repeating compilation for every new buffer were done by `nvim-treesitter`.
 -- Since then, latest upstream should be used by setting version to `false`.
+-- ! Note that this also works on any other plugins that has a very stale version release, using their latest upstream would make it better, but not really recommended.
 
 return {
 	{
@@ -14,7 +15,7 @@ return {
 	end,
 	dependencies = {
 		{ "andymass/vim-matchup", config = function() vim.g.matchup_matchparen_offscreen = { method = "popup" } end, event = "BufReadPost", version = false },
-		{ "nvim-treesitter/nvim-treesitter-context", config = true, event = "BufReadPost" }
+		{ "nvim-treesitter/nvim-treesitter-context", config = function() require("treesitter-context").setup() end, event = "BufReadPost" }
 	},
 	opts = {
 		additional_vim_regex_highlighting = false,
@@ -62,7 +63,7 @@ return {
 			scope_incremental = "grc",
 			node_decremental = "grm",
 		},
-		-- matchup = { enable = true },
+		matchup = { enable = true },
 		textobjects = { enable = true }
 	},
 	event = "BufReadPost",
