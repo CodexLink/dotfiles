cf80fe9202228e234f7deef22aa87371ecb1798c
Janrey Licas <janjan.licas@gmail.com>
2023-01-28 01:13:10 +0800

    feat(utils): initialized with context of LSP Formatting

---
+++ editors/nvim/lua/utils.lua
@@ -0,0 +1,15 @@
+-- utils.lua  | Anything created by me.
+-- @CodexLink    | https://github.com/CodexLink
+
+
+-- ! Code Formatting
+-- !!! Reference: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
+
+local lsp_formatting = function()
+	local async_provider = require("plenary.async")
+	local notify_async = require("notify").async_provider
+
+	vim.lsp.buf.format({ async = true })
+	async_provider.run(function() vim.notify("Formatting done!") end)
+end
+vim.api.nvim_create_user_command("LSPCodeFormat", lsp_formatting, {})
