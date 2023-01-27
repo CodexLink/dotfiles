a13eab808d7b857c8031e57cf2b3a127b597dc92
Janrey Licas <janjan.licas@gmail.com>
2023-01-28 02:13:38 +0800

    refactor(): removed `stylua` and `selene` + removed unnecessary keybinds + code format

--- editors/nvim/lua/plugin-specs/coding-helpers.lua
+++ editors/nvim/lua/plugin-specs/coding-helpers.lua
@@ -31,7 +31,7 @@ return {
 				experimental = {
 					ghost_text = true
 				},
-			  formatting = {
+				formatting = {
 					expandable_indicator = true,
 					fields = { "kind", "abbr", "menu" },
 					format = function(entry, vim_item)
@@ -52,8 +52,8 @@ return {
 						i = cmp.mapping.select_prev_item({ behavior = cmp_types.cmp.SelectBehavior.Select }),
 					},
 					["<M-a>"] = {
-            i = { cmp.mapping.abort() },
-          },
+						i = { cmp.mapping.abort() },
+					},
 					["<M-q>"] = cmp.mapping.scroll_docs(-3),
 					["<M-e>"] = cmp.mapping.scroll_docs(3),
 					["<Tab>"] = cmp.mapping(function(fallback)
@@ -91,12 +91,12 @@ return {
 					{ name = "path" }
 				},
 				snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
-			  window = {
+				window = {
 					completion = cmp.config.window.bordered({
-							border = "rounded",
-							scrollbar = false,
-							winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None"
-						}),
+						border = "rounded",
+						scrollbar = false,
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None"
+					}),
 					documentation = cmp.config.window.bordered({ border = "rounded" })
 				}
 			})
@@ -137,7 +137,6 @@ return {
 					null_ls.builtins.diagnostics.cppcheck,
 					null_ls.builtins.diagnostics.cpplint,
 					null_ls.builtins.diagnostics.mypy,
-					null_ls.builtins.diagnostics.selene,
 					-- * Formatters
 					null_ls.builtins.formatting.black,
 					null_ls.builtins.formatting.clang_format,
@@ -149,7 +148,6 @@ return {
 					null_ls.builtins.formatting.remark,
 					null_ls.builtins.formatting.reorder_python_imports,
 					null_ls.builtins.formatting.sql_formatter,
-					null_ls.builtins.formatting.stylua
 				}
 			})
 
@@ -179,26 +177,19 @@ return {
 			-- after the language server attaches to the current buffer
 			local on_attach = function(_, bufnr)
 				-- Enable completion triggered by <c-x><c-o>
-				vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+				vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 				-- Mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local bufopts = { noremap=true, silent=true, buffer=bufnr }
+				local bufopts = { noremap = true, silent = true, buffer = bufnr }
 				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 				vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 				vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-				vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-				vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-				vim.keymap.set('n', '<space>wl', function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, bufopts)
 				vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
 				vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 				vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
 				vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-				vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 			end
 
 			lspconfig["cssmodules_ls"].setup({
@@ -251,8 +242,17 @@ return {
 				on_attach = on_attach,
 				settings = {
 					Lua = {
-						diagnostics = {
-							globals = { "vim" }
+						completion = {
+							callSnippet = "Replace",
+							displayContext = 1
+						},
+						defaultConfig = {
+							indent_style = "tab",
+							indent_size = "2",
+						},
+						diagnostics = { globals = { "vim" } },
+						hints = {
+							enable = true
 						},
 						runtime = {
 							version = "Lua 5.4"
