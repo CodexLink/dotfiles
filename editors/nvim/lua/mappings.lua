f272ad587fcd03e756a8f1b4ea8d968c7bc365a4
Janrey Licas <janjan.licas@gmail.com>
2023-01-29 02:31:57 +0800

    refactor(mapping)!: remap + commands converted to require-able functions for lazy load

--- editors/nvim/lua/mappings.lua
+++ editors/nvim/lua/mappings.lua
@@ -5,53 +5,70 @@
 -- * I cannot do keybinds for LSP as they are required to be fed from the setup function argument `on_attach` on every LSP installed.
 -- [1] Some keybinds require using string instead of encapsulated [[]] context. This may be due to special handling after executing the function.
 
+-- [2] APIs for the plugins leveraging `telescope` are most likely not documented in terms of accessing them through APIs, the following is the command used to identify them: 'lua vim.inspect(print(require("telescope.extensions")))`
+-- [3] To reduce telescope startup time, require("telescop").load_extension was declared from the keybinds instead before initializing them.
+--
 local wk = require("which-key")
 
 wk.register({
-	["<F1>"] = { [[ <CMD>Telescope builtin<CR> ]], "telesocpe.nvim: Toggle 'builtin'" },
-	["<M-F1>"] = { [[ <CMD>Telescope file_browser<CR> ]], "telescope.nvim: Toggle 'file browser'" },
-	["<F2>"] = { [[ <CMD>TroubleToggle<CR> ]], "trouble.nvim (Diagnostics): Toggle" },
-	["<M-F2>"] = { [[ <CMD>Telescope diagnostics<CR> ]], "telescope.nvim: Toggle 'diagnostics'" },
-	["<F3>"] = { [[ <CMD>AerialToggle!<CR> ]], "symbols-outline.nvim: Toggle (Unfocused)" },
-	["<S-F3>"] = { [[ <CMD>AerialToggle<CR> ]], "symbols-outline.nvim: Toggle (Focused)" },
-	["<M-F3>"] = { [[ <CMD>Telescope aerial<CR> ]], "telescope.nvim: Toggle 'aerial'" },
-	-- ["<F4>"] = { function() print end, "DAP"},
-	["<F5>"] = { [[ <CMD>Lazy<CR> ]], "lazy.nvim: Opens UI window" },
-	["<F6>"] = { [[ <CMD>WhichKey<CR> ]], "which-key.nvim: Opens UI window for hinting keybinds" },
-	["<F7>"] = { [[ <CMD>Mason<CR> ]], "mason.nvim: Opens UI window" },
+	["<F1>"] = { function() require("telescope.builtin").builtin() end, "telesocpe.nvim: Toggle 'builtin'" },
+	["<M-F1>"] = { function() require("telescope").load_extension("file_browser") require("telescope").extensions.file_browser.file_browser() end,
+		"telescope.nvim: Toggle 'file browser'" },
+	["<F2>"] = { function() require("trouble").toggle() end, "trouble.nvim (Diagnostics): Toggle" },
+	["<M-F2>"] = { function() require("telescope.builtin").diagnostics() end, "telescope.nvim: Toggle 'diagnostics'" },
+	["<F3>"] = { function() require("aerial").toggle({ focus = false }) end, "symbols-outline.nvim: Toggle (Unfocused)" },
+	["<S-F3>"] = { function() require("aerial").toggle({ focus = true }) end, "symbols-outline.nvim: Toggle (Focused)" },
+	["<M-F3>"] = { function() require("telescope").load_extension("aerial") require("telescope").extensions.aerial.aerial() end,
+		"telescope.nvim: Toggle 'aerial'" },
+	-- ["<F4>"] = { function () print end, "DAP"},
+	["<F5>"] = { function() require("lazy").home() end, "lazy.nvim: Opens UI window" },
+	["<F6>"] = { function () require("which-key").show() end, "which-key.nvim: Opens UI window for hinting keybinds" },
+	["<F7>"] = { function () require("mason.ui").open() end, "mason.nvim: Opens UI window" },
 	["<Leader>"] = {
-		a = { [[ <CMD>Neogen<CR> ]], "neogen: Annotate code context" },
+		a = { function() require("neogen").generate() end, "neogen: Annotate code context" },
 		G = {
 			name = "gitsigns.nvim",
-			b = { [[ <CMD>Gitsigns toggle_current_line_blame<CR> ]], "Toggle line blame" },
-			d = { [[ <CMD>Gitsigns diffthis<CR> ]], "'diffthis' on current line" },
+			b = { function() require("gitsigns").toggle_current_line_blame() end, "Toggle line blame" },
+			d = { function() require("gitsigns").diffthis() end, "'diffthis' on current line" },
 			D = { function() require("gitsigns").diffthis("~") end, "`diffthis` on whole file" },
-			h = { [[ <CMD>Gitsigns preview_hunk<CR> ]], "Preview hunk (changes on certain code line)" },
+			h = { function() require("gitsigns").preview_hunk() end, "Preview hunk" },
+			H = { function() require("gitsigns").preview_hunk_inline() end, "Preview hunk (Inlined)" },
 		},
 		i = { ":IncRename ", "inc-rename.nvim: Rename on cursor" },
 		I = { function() return ":IncRename " .. vim.fn.expand("<CWORD>") end, "inc-rename.nvim: Rename by type", expr = true },
-		L = { [[ <CMD>LazyGit<CR> ]], "lazygit.nvim: Toggle window", silent = true },
-		m = { [[ <CMD>MarkdownPreviewToggle<CR> ]], "markdown-preview.nvim: Toogle" },
-		n = { [[ <CMD>Telescope notify<CR> ]], "nvim-notify: Check notifications via 'Telescope'" },
+		l = {
+			name = "LSP Actions",
+		},
+		L = { function() require("telescope").load_extension("lazygit") require("lazygit").lazygit() end, "lazygit.nvim: Toggle window" },
+		m = { [[ <CMD>MarkdownPreviewToggle<CR> ]], "markdown-preview.nvim: Toggle" },
+		n = { function() require("telescope").extensions.notify.notify() end,
+			"nvim-notify: Check notifications via 'Telescope'" },
 		r = { function() require("ssr").open() end, mode = { "n", "x" }, "ssr.nvim: Do 'Structural Search and Replace'" },
 		t = { function() require("tsht").nodes() end, "nvim-treehopper: Hop to highlight context" },
-		T = { [[ <CMD>Twilight<CR> ]], "twilight.nvim: Toggle code dimming" },
+		T = { function() require("twilight").toggle() end, "twilight.nvim: Toggle code dimming" },
 		w = {
 			name = "windows.nvim",
-			["="] = { [[ <CMD>WindowsEqualize<CR> ]], "Equalize" },
-			["+"] = { [[ <CMD>WindowsMaximize<CR> ]], "Maximize" },
-			["-"] = { [[ <CMD>WindowsMaximizeVertically<CR> ]], "Maximize vertically" },
-			["_"] = { [[ <CMD>WindowsMaximizeHorizontally<CR> ]], "Maximize horizontally" },
+			["="] = { function() require("windows.commands").equalize() end, "Equalize" },
+			["+"] = { function() require("windows.commands").maximize() end, "Maximize" },
+			["-"] = { function() require("windows.commands").maximize_vertically() end, "Maximize vertically" },
+			["_"] = { function() require("windows.commands").maximize_horizontally() end, "Maximize horizontally" },
 		}
 	},
-	["<M-<>"] = { [[ <CMD>BufferLineCyclePrev<CR> ]], "bufferline: Move to Previous Buffer." },
-	["<M->>"] = { [[ <CMD>BufferLineCycleNext<CR> ]], "bufferline: Move to Next Buffer." },
-	["<M-a>"] = { function() require("illuminate").goto_prev_reference() end, "vim-illuminate: Jump to previous reference" },
-	["<M-c>"] = { "<CMD>BufferLinePickClose<CR>", "bufferline: Pick a buffer to close." }, -- [1]
-	["<M-d>"] = { "<CMD>BufferLinePick<CR>", "bufferline: Pick a buffer to display." }, -- [1]
-	["<M-e>"] = { [[ <CMD>TSJToggle<CR> ]], "treesj: Toggle 'One-Liner/Splitted' Style." },
-	["<M-s>"] = { function() require("illuminate").goto_next_reference() end, "vim-illuminate: Jump to next reference" },
-	["<M-F>"] = { [[ <CMD>LSPCodeFormat<CR> ]], "utils: Code Format (Async)" },
-	["<S-Left>"] = { [[ <CMD>BufferLineMovePrev<CR> ]], "bufferline: Move Buffer to the Left." },
-	["<S-Right>"] = { [[ <CMD>BufferLineMoveNext<CR> ]], "bufferline: Move Buffer to the Right." }
+	["M-`"] = { function() vim.cmd([[ set wrap! ]]) end, "builtin: Toggle wrap" },
+	["<M-<>"] = { function() require("bufferline").cycle(-1) end, "bufferline: Move to Previous Buffer." },
+	["<M->>"] = { function() require("bufferline").cycle(1) end, "bufferline: Move to Next Buffer." },
+	["<M-a>"] = { function() require("hop").hint_char1() end, "hop.nvim: Hop 1 char" },
+	["<M-A>"] = { function() require("hop").hint_char2() end, "hop.nvim: Hop 2 chars" },
+	["<M-c>"] = { function() require("bufferline").close_buffer_with_pick() end, "bufferline: Pick a buffer to close." }, -- [1]
+	["<M-d>"] = { function() require("bufferline").close_buffer_with_pickfunction() require("bufferline").pick_buffer() end, "bufferline: Pick a buffer to display." }, -- [1]
+	["<M-s>"] = { function() require("hop").hint_anywhere({ direction = require("hop.hint").HintDirection.AFTER_CURSOR }) end,
+		"hop.nvim: hop below anywhere" },
+	["<M-S>"] = { function() require("hop").hint_anywhere({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR }) end,
+		"hop.nvim: hop above anywhere" },
+	["<M-v>"] = { function() require("treesj").toggle() end, "treesj: Toggle 'One-Liner/Splitted' Style." },
+	["<M-x>"] = { function() require("illuminate").goto_next_reference() end, "vim-illuminate: Jump to next reference" },
+	["<M-z>"] = { function() require("illuminate").goto_prev_reference() end, "vim-illuminate: Jump to previous reference" },
+	["<M-F>"] = { function() require("utils").LSPCodeFileFormat() end, "utils: Code Format (Async)" },
+	["<S-Left>"] = {  function() require("bufferline").move(-1) end, "bufferline: Move Buffer to the Left." },
+	["<S-Right>"] = { function() require("bufferline").move(1) end, "bufferline: :Move Buffer to the Right." }
 })
