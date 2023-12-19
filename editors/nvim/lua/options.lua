c54fa9c792cf60f8e7cc3a3dc42b59a93623e5e5
Janrey Licas <janjan.licas@gmail.com>
2023-12-20 00:51:32 +0800

    chore: upstream incomplete styling of `lualine`

--- editors/nvim/lua/options.lua
+++ editors/nvim/lua/options.lua
@@ -15,7 +15,8 @@ vo.hlsearch = false
 vo.guicursor =
 "n-v:block-CursorToNavigation,i-ci-ve:ver50-CursorToInsertion,r-c-cr-o:hor50-CursorToReplacement,a:blinkwait50-blinkoff25-blinkon25-CursorToNavigation,sm:ver50-CursorToNavigation-blinkwait25-blinkoff50-blinkon50" -- [3]
 vo.number = true
-vg.mapleader = "\\"                                                                                                                                                                                                  -- [1]
+vg.mapleader =
+"\\" -- [1]
 vo.smartindent = true
 vo.shiftwidth = 2
 vo.showmatch = true
