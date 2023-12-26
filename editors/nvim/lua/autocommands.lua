ffc8ab5b9db1f4b60eccd509b9ae3e6d758ebb1a
Janrey Licas <janjan.licas@gmail.com>
2023-12-26 18:38:19 +0800

    chore(autocmd): remove custom groups

--- editors/nvim/lua/autocommands.lua
+++ editors/nvim/lua/autocommands.lua
@@ -4,23 +4,17 @@
 
 -- Highlight yank for a short time period.
 vim.api.nvim_create_autocmd("TextYankPost",
-  { callback = function() vim.highlight.on_yank({ higroup = "HighlightedYank", timeout = 1250 }) end }
+  {
+    callback = function()
+      vim.highlight.on_yank({ higroup = "HighlightedYank", timeout = 1250 })
+    end
+  }
 )
 
-vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" },
+vim.api.nvim_create_autocmd({ "InsertLeave" },
   {
     callback = function()
       local ok, l = pcall(require, "lint")
       if ok then l.try_lint() end
     end
-  })
-
--- My own custom events, used to trigger some components without the need of triggering other plugins that depends on it.
-local custom_event = vim.api.nvim_create_augroup("CodexGroup", { clear = true })
-
-vim.api.nvim_create_autocmd("User",
-  {
-    pattern = "ForceLSPWakeup",
-    callback = function() vim.notify_once("LSP Wakeup initiated!", vim.log.levels.WARN) end,
-    group = custom_event
   })
