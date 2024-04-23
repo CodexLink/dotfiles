433963fe3a6ce6ec76835a7158f9d0523c3b65ab
Janrey Licas <janjan.licas@gmail.com>
2024-04-23 12:57:42 +0800

    feat(trackers): substitute `NTBBloodbath` -> `YannickFricke` codestats package

--- .config/nvim/lua/plugin-specs/trackers.lua
+++ .config/nvim/lua/plugin-specs/trackers.lua
@@ -1,10 +1,10 @@
----@module 'trackers'
+---@module "trackers"
 ---@author CodexLink <https://github.com/CodexLink>
 ---@license Apache-2.0
 ---@info [1] Requires `CODESTATS_API_KEY` on Environmental Variables.
 
 return {
-  { "wakatime/vim-wakatime",       event = "VeryLazy" },
-  { "andweeb/presence.nvim",       config = true,      event = "VeryLazy" },
-  { "NTBBloodbath/codestats.nvim", event = "VeryLazy", pin = true }  -- [1]
+  { "wakatime/vim-wakatime",        event = "VeryLazy" },
+  { "andweeb/presence.nvim",        config = true,                                             event = "VeryLazy" },
+  { "YannickFricke/codestats.nvim", config = function() require("codestats-nvim").setup() end, dependencies = "nvim-lua/plenary.nvim", event = "VeryLazy" } -- [1]
 }
